d470531104a487a21aa47f0d4d4373e17399fe88
mark_kibo <kibochamark@gmail.com>
2023-09-30 13:40:20 +0300

    completed code challenge

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,21 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, exists, create_engine, Column, Integer, String, MetaData, Table
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+from datetime import datetime
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
-
+engine = create_engine('sqlite:///freebies.db')
 Base = declarative_base(metadata=metadata)
+sesssion=sessionmaker(bind=engine)()
+    
+association_table=Table(
+    'association',
+    Base.metadata,
+    Column("dev_id", ForeignKey("devs.id")),
+    Column("company_id", ForeignKey("companies.id"))
+)
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -15,7 +23,28 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
-
+    freebies =relationship('Freebie', backref="freebies")
+    devs = relationship('Dev', secondary=association_table, back_populates="companies")
+    
+    def give_freebie(self, devI, name, value):
+        
+        dev=sesssion.query(Dev).filter(Dev.name == devI.name).first()
+        freebie_exist=sesssion.query(Freebie).filter(Freebie.dev == dev.id).exists()
+        print(freebie_exist)
+        if freebie_exist is None:
+            freebie=Freebie(item_name=name, value=value, dev=dev.id, company=self.id)
+            # Add the Freebie instance to the Company's freebies relationship
+            print(freebie)
+            self.freebies.append(freebie)
+            sesssion.add(freebie)
+            sesssion.commit()
+            return "created"
+        return "there exists that freebie"
+    
+    def oldest_company():
+        company=sesssion.query(Company).filter(Company.founding_year < int(datetime.now().year)).order_by(Company.founding_year.asc()).first()
+        print(company.founding_year)
+        return company
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -24,6 +53,42 @@ class Dev(Base):
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
+    freebies =relationship('Freebie', backref="freebies_instances")
+    companies = relationship('Company', secondary=association_table, back_populates="devs")
 
+    def received_one(self, item_name):
+        freebies=sesssion.query(Freebie).filter(Freebie.item_name == item_name).all()
+        if freebies is not None:
+            for x in freebies:
+                if x.dev == self.id:
+                    return True
+        return False
+    
+    def give_away(self, dev, freebie):
+        freebieObj=sesssion.query(Freebie).filter(Freebie.item_name == freebie.item_name).first()
+        if freebieObj.dev == self.id:
+            freebieObj.dev = self.id
+            sesssion.commit()
+            return "successful"
+        return "Freebie doesn't belong to this dev"
+    
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+
+
+class Freebie(Base):
+    __tablename__ ="freebies"
+
+
+    id=Column(Integer(), primary_key=True)
+    item_name=Column(String(50), nullable=True)
+    value=Column(Integer())
+
+    dev=Column(Integer(), ForeignKey('devs.id'))
+    company=Column(Integer(), ForeignKey('companies.id'))
+
+
+
+    def print_details(self):
+        return f"{self.dev} owns a {self.item_name} from {self.company}"
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,62 @@
-#!/usr/bin/env python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
 
-# Script goes here!
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # # Create instances of Company, Dev, and Freebie
+    # company1 = Company(name='Company 1', founding_year=2000)
+    # company2 = Company(name='Company 2', founding_year=1995)
+
+   
+
+    dev1 = Dev(name='Developer 1')
+    # dev2 = Dev(name='Developer 2')
+
+    
+    # session.add(company1)
+    # session.add(company2)
+    # session.add(dev1)
+    # session.add(dev2)
+    dev=session.query(Dev).filter(Dev.name == "Developer 1").first()
+    company=session.query(Company).filter(Company.name == "Company 1").first()
+    freebie1 = Freebie(item_name='Item 1', value=100, dev=dev.id, company=company.id)
+    # # freebie2 = Freebie(item_name='Item 2', value=200, dev=dev2, Company=company2)
+    # # Add instances to the session
+    # print(dev, company, freebie1)
+    
+    # session.add(freebie1)
+    # session.add(freebie2)
+
+    
+    # Associate data by setting relationships
+    # company1.devs.append(dev1)
+    # company1.freebies.append(freebie1)
+
+
+    # dev1.companies.append(company1)
+    # dev1.freebies.append(freebie1)  # Renamed the backref
+
+    
+
+    
+    
+    # Commit the changes to the database
+    # session.commit()
+
+    # Close the session
+    # session.close()
+    # dev3 = Dev(name='Developer 3')
+    # session.add(dev3)
+    # company=session.query(Company).filter(Company.name == "Company 1").first()
+    # print(company.name)
+    # company.give_freebie(dev1, "item 3", 400)
+
+    dev=session.query(Dev).filter(Dev.name == "Developer 1").first()
+
+    dev3 = Dev(name='Developer 3')
+    
+    print(dev.give_away(dev3, freebie1))
